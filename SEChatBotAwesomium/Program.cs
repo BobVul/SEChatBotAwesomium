1832c36a2153ed028508d7fda73e957f1f4b335b
Bob <elusive138+git@gmail.com>
2013-10-06 11:51:34 +1100

    Login achieved

--- SEChatBotAwesomium/Program.cs
+++ SEChatBotAwesomium/Program.cs
@@ -12,11 +12,18 @@ namespace SEChatBotAwesomium
     {
         static WebSession aweSession = null;
         static WebView aweView = null;
+        static bool pageLoaded = false;
+        static bool frameLoaded = false;
+        static System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
 
         static void Main(string[] args)
         {
             try
             {
+                WebConfig config = new WebConfig();
+                config.UserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2";
+                WebCore.Initialize(config);
+
                 WebPreferences prefs = new WebPreferences();
                 prefs.LoadImagesAutomatically = true;
                 prefs.RemoteFonts = false;
@@ -29,14 +36,17 @@ namespace SEChatBotAwesomium
                 prefs.LocalStorage = true;
                 prefs.Databases = false;            // ?
 
-                aweSession = WebCore.CreateWebSession("session.dat", prefs);
+                aweSession = WebCore.CreateWebSession("session", prefs);
                 aweSession.ClearCookies();
                 aweView = WebCore.CreateWebView(1024, 768, aweSession);
                 aweView.ResponsiveChanged += aweView_ResponsiveChanged;
                 aweView.Crashed += aweView_Crashed;
                 aweView.ConsoleMessage += aweView_ConsoleMessage;
+                aweView.LoadingFrameComplete += aweView_LoadingFrameComplete;
 
-                
+                string[] lines = File.ReadAllLines("account.txt");
+                sw.Start();
+                go(lines[0], lines[1], "master.min.js", "http://chat.stackexchange.com/rooms/118/root-access");
             }
             finally
             {
@@ -58,7 +68,23 @@ namespace SEChatBotAwesomium
 
         static void aweView_ConsoleMessage(object sender, ConsoleMessageEventArgs e)
         {
-            Console.WriteLine(e.ToString());
+            Console.WriteLine("{0}: {1} {2} {3}: {4}", e.EventType, e.EventName, e.Source, e.LineNumber, e.Message);
+        }
+
+        static void aweView_LoadingFrameComplete(object sender, FrameEventArgs e)
+        {
+            if (e.IsMainFrame)
+            {
+                BitmapSurface surface = (BitmapSurface)aweView.Surface;
+                surface.SaveToPNG("screenshot" + sw.ElapsedMilliseconds + ".png");
+                pageLoaded = true;
+            }
+            else
+            {
+                BitmapSurface surface = (BitmapSurface)aweView.Surface;
+                surface.SaveToPNG("screenshot" + sw.ElapsedMilliseconds + "f.png");
+                frameLoaded = true;
+            }
         }
 
         static void go(String s_username, String s_password, String scriptPath, String chatURL)
@@ -67,14 +93,15 @@ namespace SEChatBotAwesomium
             {
                 throw new UriFormatException("The provided chat URL \"" + chatURL + "\" is invalid!");
             }
+            pageLoaded = false;
             aweView.Source = chatURL.ToUri();
-            while (aweView.IsLoading)
+            while (!pageLoaded)
             {
                 WebCore.Update();
             }
 
-            Console.WriteLine(aweView.Source + ": Chatroom loaded! Waiting for the chat JS.");
-            Thread.Sleep(5000);
+            //Console.WriteLine(aweView.Source + ": Chatroom loaded! Waiting for the chat JS.");
+            //Thread.Sleep(5000);
 
             Console.WriteLine(aweView.Source + ": In the chatroom! Determining if we need to login...");
             // ImagePanel.displayImage(dri.getScreenshotAs(OutputType.BASE64));
@@ -89,6 +116,7 @@ namespace SEChatBotAwesomium
             {
                 Console.WriteLine("Crap. We need to login.");
                 string link = loginLink.ToString();
+                pageLoaded = false;
                 aweView.Source = link.ToUri();
             }
             else
@@ -99,7 +127,7 @@ namespace SEChatBotAwesomium
             if (loginLink != null)
             {
                 //Need to login
-                while (aweView.IsLoading)
+                while (!pageLoaded)
                 {
                     WebCore.Update();
                 }
@@ -112,7 +140,8 @@ namespace SEChatBotAwesomium
                 {
                     Console.WriteLine("We don't have a network cookie at all.");
                     string link = midpointLink.ToString();
-                    aweView.Source = link.ToUri(); ;
+                    pageLoaded = false;
+                    aweView.Source = link.ToUri();
                 }
                 else
                 {
@@ -120,7 +149,7 @@ namespace SEChatBotAwesomium
                 }
                 if(midpointLink != null)
                 {
-                    while (aweView.IsLoading)
+                    while (!pageLoaded)
                     {
                         WebCore.Update();
                     }
@@ -130,51 +159,75 @@ namespace SEChatBotAwesomium
                     // TODO: only works up to around here
 
 
+                    frameLoaded = false;
                     aweView.ExecuteJavascriptWithResult("openid.signin('stack_exchange');");
-                    // Perhaps use the .LoadingFrameComplete event instead?
-                    while (aweView.IsLoading)
+                    while (!frameLoaded)
                     {
                         WebCore.Update();
                     }
-                    Thread.Sleep(5000);
                     // ImagePanel.displayImage(dri.getScreenshotAs(OutputType.BASE64));
                     Console.WriteLine(aweView.Source + ": We should be at the sign-in page now...");
 
-                    BitmapSurface surface = (BitmapSurface)aweView.Surface;
-                    surface.SaveToPNG("screenshot.png");
+                    frameLoaded = false;
+                    while (!frameLoaded)
+                    {
+                        WebCore.Update();
+                    }
 
                     document = (JSObject)aweView.ExecuteJavascriptWithResult("document", "//iframe[@id='affiliate-signin-iframe']");
                     document.getElementById("email").value = s_username;
                     document.getElementById("password").value = s_password;
                     // ImagePanel.displayImage(dri.getScreenshotAs(OutputType.BASE64));
-                    dynamic submitButton = document.getElementsByClassName("affiliate-button");
+
+                    //aweView.ExecuteJavascriptWithResult("var evt = document.createEvent('HTMLEvents'); evt.initEvent('click', true, true ); document.getElementsByClassName('affiliate-button')[0].dispatchEvent(evt); ");
+                    dynamic submitButton = document.getElementsByClassName("affiliate-button")[0];
                     if(submitButton == null)
                         throw new Exception("Couldn't find the affiliate-button classed submit button!");
+                    pageLoaded = false;
                     submitButton.click();
-                    while (aweView.IsLoading)
+                    while (!pageLoaded)
                     {
                         WebCore.Update();
                     }
-                    Thread.Sleep(5000);
                     Console.WriteLine(aweView.Source + ": We should be logged in now...");
                     // ImagePanel.displayImage(dri.getScreenshotAs(OutputType.BASE64));
 
+                    pageLoaded = false;
                     aweView.Source = chatURL.ToUri();
-                    while (aweView.IsLoading)
+                    while (!pageLoaded)
                     {
                         WebCore.Update();
                     }
-                    Console.WriteLine(aweView.Source + ": Chatroom loaded! Waiting for the chat JS.");
-                    Thread.Sleep(5000);
+                    //Console.WriteLine(aweView.Source + ": Chatroom loaded! Waiting for the chat JS.");
+                    //Thread.Sleep(5000);
 
                     Console.WriteLine(aweView.Source + ": In the chatroom! Authenticating...");
                     // ImagePanel.displayImage(dri.getScreenshotAs(OutputType.BASE64));
 
-                    loginLink = aweView.ExecuteJavascriptWithResult("document.evaluate(\"//a[starts-with(@href, '/login/global') and text() = 'logged in' and not(ancestor::div[contains(@style,'display:none')]) and not(ancestor::div[contains(@style,'display: none')])]\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;");
-                    loginLink.Invoke("click");
-                    Thread.Sleep(8000);
+                    Thread.Sleep(3000);
+
+
+                    BitmapSurface surface = (BitmapSurface)aweView.Surface;
+                    surface.SaveToPNG("screenshot" + sw.ElapsedMilliseconds + ".png");
+
+                    document = (JSObject)aweView.ExecuteJavascriptWithResult("document");
+                    loginLink = document.evaluate("//a[starts-with(@href, '/login/global') and text() = 'logged in' and not(ancestor::div[contains(@style,'display:none')]) and not(ancestor::div[contains(@style,'display: none')])]", document, null, 9 /*XPathResult.FIRST_ORDERED_NODE_TYPE*/, null).singleNodeValue;
+                    if (loginLink != null)
+                    {
+                        string link = loginLink.ToString();
+                        pageLoaded = false;
+                        aweView.Source = link.ToUri();
+                        while (!pageLoaded)
+                        {
+                            WebCore.Update();
+                        }
+                    }
                 }
             }
+
+
+            Thread.Sleep(5000);
+            ((BitmapSurface)aweView.Surface).SaveToPNG("screenshot" + sw.ElapsedMilliseconds + ".png");
             
             String content = File.ReadAllText(scriptPath);
             if (content == null || content.Length == 0)
